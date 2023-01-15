3023ed22574474b249dc99ccceb2435fbbe9657d
Marcos653 <viniciustatuta@gmail.com>
2023-01-15 18:44:42 -0300

    feat: set up cors

---
+++ src/main/java/book/config/Cors.java
@@ -0,0 +1,14 @@
+package book.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Cors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
